38307695a47d1f70346f4dcd7b6326bda1e02a26
Niels Weistra <n.weistra@itlusions.nl>
2025-03-25 22:00:47 +0100

    Add function

--- src/v1/routers/resources.py
+++ src/v1/routers/resources.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Query
 from typing import Optional
 from v1.controllers.resourceexplorer.controller import (
     get_all_namespaces as controller_get_namespaces,
+    get_all_secrets as controller_get_secrets
 )
 from v1.models.models import ExampleModel as ModelVoorbeeld
 
