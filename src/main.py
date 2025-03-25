33d0c4685c0f7ad2fc62b2f1bee97470df1e6955
Niels Weistra <n.weistra@itlusions.nl>
2025-03-26 00:20:21 +0100

    Add router to application

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import RedirectResponse, JSONResponse
 from base.routers import router as base_router
 from base.k8s_config import load_k8s_config
 from v1.routers.resources import router as v1_resources_router
-
+from v1.routers.describe import router as v1_describe_router
 
 load_k8s_config()
 
@@ -12,6 +12,7 @@ app_v1 = FastAPI()
 
 app.include_router(base_router, tags=["Health"])
 app_v1.include_router(v1_resources_router)
+app_v1.include_router(v1_describe_router)
 
 app.mount("/v1", app_v1)
 
