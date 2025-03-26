45fb05c359aa7548032404e2b4584ba9f63d5628
Niels Weistra <n.weistra@itlusions.nl>
2025-03-26 01:02:34 +0100

    Update

--- src/v1/routers/describe.py
+++ src/v1/routers/describe.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from kubernetes.client.exceptions import ApiException
-from models import ResourceDetail  # Import from models.py
+from v1.models.models import ResourceDetail
 from v1.controllers.resourceexplorer.describe import (
     describe_resource as controller_describe_resource,
 )
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/describe", tags=["Describe Resources"])
 @router.get("/{namespace}/{resource_type}/{resource_name}", response_model=ResourceDetail)
 async def describe_resource(namespace: str, resource_type: str, resource_name: str):
     try:
-        resource_detail = controller_describe_resource(namespace, resource_type, resource_name)
+        resource_detail = await controller_describe_resource(namespace, resource_type, resource_name)
         return resource_detail
     except ApiException as e:
         raise HTTPException(status_code=e.status, detail=e.reason)
