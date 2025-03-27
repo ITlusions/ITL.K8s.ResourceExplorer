98ce5335d0febf34cf010093e512e79346593274
Niels Weistra <n.weistra@itlusions.nl>
2025-03-27 21:39:13 +0100

    Add CRD router

---
+++ src/v1/routers/crd.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from v1.controllers.crd import list_crds, get_crd_items
+from v1.models.models import CRDItemRequest
+
+router = APIRouter()
+
+@router.get("/crds", tags=["CRDs"])
+def list_all_crds():
+    """
+    API endpoint to list all Custom Resource Definitions (CRDs).
+    """
+    return list_crds()
+
+@router.post("/crds/items", tags=["CRDs"])
+def get_items_from_crd(request: CRDItemRequest):
+    """
+    API endpoint to get items from a specific CRD.
+    """
+    return get_crd_items(
+        group=request.group,
+        version=request.version,
+        plural=request.plural,
+        namespace=request.namespace,
+    )
