d6f41eaadf0e92e4183bd0f87f0dc1ec058a2c88
Niels Weistra <n.weistra@itlusions.nl>
2025-03-27 22:11:08 +0100

    Update route

--- src/v1/routers/crd.py
+++ src/v1/routers/crd.py
@@ -2,16 +2,16 @@ from fastapi import APIRouter, Depends
 from v1.controllers.crd import list_crds, get_crd_items
 from v1.models.models import CRDItemRequest
 
-router = APIRouter()
+router = APIRouter(prefix="/crds", tags=["Describe Resources"])
 
-@router.get("/crds", tags=["CRDs"])
+@router.get("/list-crds", tags=["CRDs"])
 def list_all_crds():
     """
     API endpoint to list all Custom Resource Definitions (CRDs).
     """
     return list_crds()
 
-@router.post("/crds/items", tags=["CRDs"])
+@router.post("/items", tags=["CRDs"])
 def get_items_from_crd(request: CRDItemRequest):
     """
     API endpoint to get items from a specific CRD.
