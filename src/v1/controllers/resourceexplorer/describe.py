08bfd5a160d912a3139155efa52eb833b699a148
Niels Weistra <n.weistra@itlusions.nl>
2025-03-28 00:39:21 +0100

    Async issues

--- src/v1/controllers/resourceexplorer/describe.py
+++ src/v1/controllers/resourceexplorer/describe.py
@@ -9,14 +9,14 @@ load_k8s_config()
 core_v1_api = client.CoreV1Api()
 apps_v1_api = client.AppsV1Api()
 
-def describe_resource(namespace: str, resource_type: str, resource_name: str):
+async def describe_resource(namespace: str, resource_type: str, resource_name: str):
     try:
         if resource_type == "pod":
-            resource = core_v1_api.read_namespaced_pod(name=resource_name, namespace=namespace)
+            resource = await core_v1_api.read_namespaced_pod(name=resource_name, namespace=namespace, async_req=True)
         elif resource_type == "service":
-            resource = core_v1_api.read_namespaced_service(name=resource_name, namespace=namespace)
+            resource = await core_v1_api.read_namespaced_service(name=resource_name, namespace=namespace, async_req=True)
         elif resource_type == "deployment":
-            resource = apps_v1_api.read_namespaced_deployment(name=resource_name, namespace=namespace)
+            resource = await apps_v1_api.read_namespaced_deployment(name=resource_name, namespace=namespace, async_req=True)
         else:
             raise HTTPException(status_code=400, detail=f"Unsupported resource type: {resource_type}")
 
@@ -30,21 +30,21 @@ def describe_resource(namespace: str, resource_type: str, resource_name: str):
     
 
 
-def list_resources_grouped_by_namespace():
+async def list_resources_grouped_by_namespace():
     """
     Fetch all resources (pods, services, deployments) grouped by namespace.
     """
     try:
-        namespaces = core_v1_api.list_namespace().items
+        namespaces = (await core_v1_api.list_namespace(async_req=True)).items
         result = {}
 
         for ns in namespaces:
             namespace_name = ns.metadata.name
 
             # Fetch resources in the namespace
-            pods = core_v1_api.list_namespaced_pod(namespace=namespace_name).items
-            services = core_v1_api.list_namespaced_service(namespace=namespace_name).items
-            deployments = apps_v1_api.list_namespaced_deployment(namespace=namespace_name).items
+            pods = (await core_v1_api.list_namespaced_pod(namespace=namespace_name, async_req=True)).items
+            services = (await core_v1_api.list_namespaced_service(namespace=namespace_name, async_req=True)).items
+            deployments = (await apps_v1_api.list_namespaced_deployment(namespace=namespace_name, async_req=True)).items
 
             # Group resources by namespace
             result[namespace_name] = {
