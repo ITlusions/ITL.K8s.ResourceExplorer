9bc707aaf583918a2b1867ca72d1ffa435812501
Niels Weistra <n.weistra@itlusions.nl>
2025-04-02 12:48:47 +0200

    Update moedl

--- src/v1/models/pypi.py
+++ src/v1/models/pypi.py
@@ -6,6 +6,8 @@ class ArtifactoryTestRequest(BaseModel):
         default="https://pypi.org/simple", 
         description="The URL of the PyPI repository (default: https://pypi.org/simple)"
     )
-    username: Optional[str] = Field(None, description="Username for authentication (optional)")
+    username: Optional[str] = Field(
+        default=True, 
+        description="Username for authentication (optional)")
     password: Optional[str] = Field(None, description="Password for authentication (optional)")
     skip_tls_verify: bool = Field(False, description="Skip TLS certificate verification (default: False)")
