c189e2fd3acf8f7331634b99e068125698790f44
Niels Weistra <n.weistra@itlusions.nl>
2025-04-11 02:07:37 +0200

    use groupname

--- src/v1/routers/crd.py
+++ src/v1/routers/crd.py
@@ -30,9 +30,9 @@ def get_items_from_crd(request: CRDItemRequest):
     )
 
 # Dynamically create and add CRD routes
-for function_name, function in controller_create_dynamic_crd_functions().items():
+for group_name, function in controller_create_dynamic_crd_functions().items():
     # Dynamically create a generic model for each CRD
-    crd_model_name = f"{function_name.capitalize()}Model"
+    crd_model_name = f"{group_name.capitalize()}Model"
     crd_model = create_model(
         crd_model_name,
         apiVersion=(Optional[str], None),
@@ -45,41 +45,41 @@ for function_name, function in controller_create_dynamic_crd_functions().items()
         __base__=BaseModel,
     )
 
-    if function_name.startswith("list_"):
+    if group_name.startswith("list_"):
         if "namespace" in function.__code__.co_varnames:
             # Namespaced CRD
             router.add_api_route(
-                f"/{function_name[5:]}/{{namespace}}",
+                f"/{group_name[5:]}/{{namespace}}",
                 function,
                 methods=["GET"],
-                name=f"List {function_name[5:]}",
+                name=f"List {group_name[5:]}",
                 response_model=crd_model,
             )
         else:
             # Non-namespaced CRD
             router.add_api_route(
-                f"/{function_name[5:]}",
+                f"/{group_name[5:]}",
                 function,
                 methods=["GET"],
-                name=f"List {function_name[5:]}",
+                name=f"List {group_name[5:]}",
                 response_model=crd_model,
             )
-    elif function_name.startswith("get_"):
+    elif group_name.startswith("get_"):
         if "namespace" in function.__code__.co_varnames:
             # Namespaced CRD
             router.add_api_route(
-                f"/{function_name[4:]}/{{namespace}}/{{name}}",
+                f"/{group_name[4:]}/{{namespace}}/{{name}}",
                 function,
                 methods=["GET"],
-                name=f"Get {function_name[4:]}",
+                name=f"Get {group_name[4:]}",
                 response_model=crd_model,
             )
         else:
             # Non-namespaced CRD
             router.add_api_route(
-                f"/{function_name[4:]}/{{name}}",
+                f"/{group_name[4:]}/{{name}}",
                 function,
                 methods=["GET"],
-                name=f"Get {function_name[4:]}",
+                name=f"Get {group_name[4:]}",
                 response_model=crd_model,
             )
